55e88fc382afba167b6d3466d9e97439a3dee974
MM <you@example.com>
2024-09-25 09:21:35 +0530

    modified:   app/page.tsx 	new file:   components/loader.tsx 	new file:   site/styles.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import path from "path";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 import confetti from "canvas-confetti";
-
+import { Loader } from "@/components/loader";
+import { styles } from "@/site/styles";
 // https://www.leetcode.com 
 
 export const fetchCache = "force-no-store";
@@ -15,19 +16,23 @@ export const dynamic = 'force-static';
 export default function Home() {
   const [url, setUrl] = useState("");
   const [dummyUrl, setDummyUrl] = useState("");
+  const [isDisabled, setDisabled] = useState(false);
   const query = useSearchParams();
   const router = useRouter();
 
   useEffect(() => {
     setDummyUrl("");
     router.push("/");
-
+    setDisabled(false);
   }, []);
 
   useEffect(() => {
     const s = query.get("short_url") || "";
     setDummyUrl(s)
-    if (s != "") handleClick()
+    if (s != "") {
+      handleClick()
+      setDisabled(false);
+    }
   }, [query]);
 
   const handleClick = () => {
@@ -39,7 +44,7 @@ export default function Home() {
   };
 
   const submit = () => {
-
+    setDisabled(true);
     if (url !== "") {
       setDummyUrl("Loading...");
       router.push(`/a?v=${url}`);
@@ -47,7 +52,7 @@ export default function Home() {
     }
     else {
       alert("No link Recieved !!");
-      window.location.reload();
+      setDisabled(false);
     }
   }
 
@@ -55,7 +60,7 @@ export default function Home() {
     <Suspense>
       <div className="flex flex-col h-screen">
         <nav className="flex">
-          <button className="text-2xl p-10 bg-gradient-to-r from-blue-500 to-orange-500 inline-block text-transparent bg-clip-text drop-shadow-2xl" onClick={() => window.location.reload()}> URL Shortener Tool </button>
+          <button className={styles.logoStyle} onClick={() => window.location.reload()}> URL Shortener Tool </button> {/* logoStyle*/}
         </nav>
         <div className="flex flex-1 flex-col justify-center items-center gap-2">
           <div className="flex gap-2">
@@ -63,15 +68,16 @@ export default function Home() {
               aria-label="Enter Url"
               placeholder="Enter Url..."
               //className="w-[250px] text-white outline-none border-4 bg-slate-400  border-purple-600 rounded-full hover:bg-slate-400/85 focus:pl-2 focus:tracking-widest focus:bg-slate-400/80"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 tracking-[0.25rem] block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className={styles.inputStyle}
               onChange={(e: any) => { e.preventDefault(); setUrl(e.target.value) }}
             />
             <button
-              className="relative inline-flex items-stretch justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+              disabled={isDisabled}
+              className={!isDisabled ? styles.buttonStyle : styles.disablesButtonStyle}
               onClick={submit}
             >
               <span className="relative px-10 py-3 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 text-base h-full">
-                Generate URL
+                {!isDisabled ? "Generate URL" : <Loader />}
               </span>
             </button>
           </div>
---
+++ site/styles.tsx
@@ -0,0 +1,7 @@
+export const styles = {
+    buttonStyle: "relative inline-flex items-stretch justify-center p-0.5 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800",
+    disablesButtonStyle: "focus:ring-purple-200",
+
+    logoStyle: "text-2xl p-10 bg-gradient-to-r from-blue-500 to-orange-500 inline-block text-transparent bg-clip-text drop-shadow-2xl",
+    inputStyle: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 tracking-[0.25rem] block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+};
