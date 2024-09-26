6d78b7b9fe1f1c82595eaee549985ea33fc0b8ea
MM <you@example.com>
2024-09-26 10:03:19 +0530

    modified:   app/(server)/[slug]/route.tsx

--- app/(server)/[slug]/route.tsx
+++ app/(server)/[slug]/route.tsx
@@ -1,31 +1,25 @@
+
 import { connect } from "../db/connection";
 
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-    request: Request,
+    request: NextRequest,
     { params }: { params: { slug: string } }
 ) {
     try {
         const db = await connect();
         const collection = await db.collection("urls");
-
-        // const stringUrl = req.url;
         const dummyUrl: string = params.slug || "";
-        console.log(dummyUrl);
+        
         const data = await collection.findOne({ short_url: dummyUrl });
 
         if (data != undefined)
             return NextResponse.redirect(data["orig_url"]);
         else
-            return NextResponse.json({ message: "Url not found", status: 500 }, { status: 500 })
+            return NextResponse.redirect(new URL(`${request.nextUrl.origin}/expired`));
+        //return NextResponse.json({ message: "Url expired !!", status: 500 }, { status: 500 })
     } catch (e) {
         return NextResponse.json({ message: "Internal Server Error", status: 500 }, { status: 500 })
     }
 }
-
-
-
-
-
-
