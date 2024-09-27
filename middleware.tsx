867896a4bdd2562be6ddaa9ea7d377ba78b64fdb
MM <you@example.com>
2024-09-27 22:47:27 +0530

    modified:   middleware.tsx

--- middleware.tsx
+++ middleware.tsx
@@ -3,26 +3,29 @@ import type { NextRequest } from 'next/server';
 import { getUrl } from "@/app/(server)/server";
 
 export async function middleware(request: NextRequest) {
-    console.log(request.nextUrl.pathname.startsWith("/a"));
-    if (request.nextUrl.pathname.startsWith("/a")) {
-        const url = request.nextUrl;
-        const pathname = url.origin + "/getUrl";
-        const orig_url: string = url.searchParams.get("v") || "";
+    try {
+        if (request.nextUrl.pathname.startsWith("/a")) {
+            const url = request.nextUrl;
+            const pathname = url.origin + "/getUrl";
+            const orig_url: string = url.searchParams.get("v") || "";
 
-        const res = await getUrl(pathname, orig_url);
-        let data;
-        if (res.status == 200)
-            data = res.slug;
-        else
-            data = res.message;
+            const res = await getUrl(pathname, orig_url);
+            let data;
+            if (res.status == 200)
+                data = res.slug;
+            else
+                data = res.message;
 
-        return NextResponse.redirect(new URL(`/?short_url=${data}`, request.url));
+            return NextResponse.redirect(new URL(`/?short_url=${data}`, request.url));
 
-    }
+        }
 
-    else {
-        console.log(false);
-        return NextResponse.redirect(new URL("/", request.url));
+        else {
+            console.log(false);
+            return NextResponse.redirect(new URL("/", request.url));
+        }
+    } catch (e) {
+        return NextResponse.redirect("/");
     }
     //return NextResponse.redirect(new URL(`/getUrl?v=${request.url}`));
 }
