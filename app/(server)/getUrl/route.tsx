e19f76671d5a3c7401fd05f9179d4e32bec8353d
MM <you@example.com>
2024-09-27 22:26:43 +0530

    modified:   app/(server)/getUrl/route.tsx 	modified:   middleware.tsx

--- app/(server)/getUrl/route.tsx
+++ app/(server)/getUrl/route.tsx
@@ -16,9 +16,9 @@ export async function POST(req: Request, res: any) {
         const date = new Date();
         //
         try {
-            console.log("try")
+            // console.log("try")
             await collection.createIndex({ createdAt: 1 }, { expireAfterSeconds: 10 });
-        } catch (e) { console.log("catch") }
+        } catch (e) { /* console.log("catch")*/ }
         await collection.insertOne({
             orig_url: orig_url,
             short_url: short_url,
@@ -30,7 +30,7 @@ export async function POST(req: Request, res: any) {
         })
         return NextResponse.json({ slug: short_url, status: 200 }, { status: 200 });
     } catch (e) {
-        console.log(e)
+        // console.log(e)
         return NextResponse.json({ message: "Internal Server Error !!! ", status: 500, err: e }, { status: 500 });
     }
 }
--- middleware.tsx
+++ middleware.tsx
@@ -32,6 +32,6 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ["/a"]
+    matcher: "/a"
 }
 
